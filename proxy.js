54e0aae6b3ad065f961bf020399cd74f8156ae71
Ömer Çakı <omerfarukcaki@gmail.com>
2021-02-02 18:43:09 +0300

    Report & final version

--- proxy.js
+++ proxy.js
@@ -23,6 +23,7 @@ if (cluster.isMaster) {
       try {
         const request = buffer.toString();
         const [method, url, protocol] = request.split(/\r\n/g)[0].split(' ');
+        console.log({ method, url });
         const body = request.split(/\r\n/g).slice(1).join('\n');
         if (url.includes('http://')) {
           // print information
@@ -98,6 +99,48 @@ if (cluster.isMaster) {
               console.log(err);
             }
           });
+        } else if (isRelativeURL(url)) {
+          // if the client requests a relative URL (such as GET /500 HTTP/1.0) from the proxy,
+          // proxy will direct this request to your web server as default.
+
+          // create a client
+          const client = new net.Socket();
+
+          // if port is undefined use 80 instead, which is the default port and not available specially on the request
+          // BONUS PART: HTTP requests to any external server is also being proxied
+          client.connect(8080, '127.0.0.1', (cc) => {
+            console.log('Connected to destination server');
+
+            // proxy server itself acting like a client to the destination server
+            client.write(`${method} ${url} HTTP/1.1\n${body}`);
+            console.log('HTTP message is seny to the destination server.');
+          });
+
+          // Reponse received from destination server
+          client.on('data', (data) => {
+            console.log(`Data received from server:`);
+            console.log(`============================\n${buffer.toString()}`);
+
+            socket.write(data);
+            socket.end();
+
+            // Save the requested object into cache for further use
+            save({ name: url.slice(1), stream: data });
+
+            client.destroy();
+            console.log('client destroyed');
+          });
+
+          client.on('error', (err) => {
+            if (err.code == 'ECONNREFUSED') {
+              // Web Server is not running currently
+              // return a “Not Found” error message with status code 404.
+              socket.write('HTTP/1.1 404 Not Found\n\n');
+              socket.end();
+            } else {
+              console.log(err);
+            }
+          });
         } else {
           // Some other protocol other than http, might be https etc.
           console.log('Not an HTTP request, skipping...');
@@ -146,4 +189,9 @@ function isGreaterThan99(size) {
 
 function isEven(path) {
   return parseInt(path) % 2 === 0 ? true : false;
+}
+
+// if the request url is relative
+function isRelativeURL(url) {
+  return url.startsWith('/');
 }
